7bcc9684f952cd685060a5a0f6626caf940cf7f0
William <cobbman@gmail.com>
2024-03-26 16:37:06 -0600

    keybinding tmux window change to vim keys

---
+++ .config/lvim/config.lua
@@ -0,0 +1,25 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+
+-- Angular support
+-- Ref: https://www.lunarvim.org/docs/features/supported-frameworks/angular
+require("lvim.lsp.manager").setup("angularls")
+
+lvim.plugins = {
+
+  { "nvim-treesitter/nvim-treesitter-angular" },
+  {
+    'wfxr/minimap.vim',
+    build = "cargo install --locked code-minimap",
+    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
+
+}
--- .zshrc
+++ .zshrc
@@ -120,7 +120,7 @@ alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
 alias nvim-kick="NVIM_APPNAME=kickstart nvim"
 alias nvim-chad="NVIM_APPNAME=NvChad nvim"
 alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
-alias nvim-lunar="NVIM_APPNAME=LunarVim nvim"
+alias nvim-lunar="NVIM_APPNAME=lvim nvim"
 
 function nvims() {
   items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim" "LunarVim")
