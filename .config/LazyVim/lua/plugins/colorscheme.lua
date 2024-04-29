144318680d847f5308bdc8f537f3b8dfeed08120
William <cobbman@gmail.com>
2024-04-29 16:34:12 -0600

    Add plugins for LazyVim

---
+++ .config/LazyVim/lua/plugins/colorscheme.lua
@@ -0,0 +1,53 @@
+return {
+  -- add catppuccin. Options are here: http://www.lazyvim.org/plugins/colorscheme#catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+  -- configure LazyVim to load catppuccin theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- color variations: catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
