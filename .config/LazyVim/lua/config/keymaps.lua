98b1297ae1d6052eee3ccc08f16fdc56b229ce9b
cobbman <cobbman@gmail.com>
2024-05-08 14:30:59 -0600

    LazyVim -> add ToggleTerm key bindings

--- .config/LazyVim/lua/config/keymaps.lua
+++ .config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- ToggleTerm
+map("n", "<leader>bT", ":ToggleTerm direction=float <CR>", { desc = "Open terminal" })
+map("n", "<leader>bt", ":ToggleTerm direction=horizontal <CR>", { desc = "Open horizontal terminal split" })
