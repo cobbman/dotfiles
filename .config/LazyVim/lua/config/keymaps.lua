07e62a167e9f3cbde4f9ae36cf8da96dc6643bbf
William Cobb <cobbman@gmail.com>
2024-11-21 11:23:55 -0700

    in LazyVim, add keymap for opening previous Telescope session

--- .config/LazyVim/lua/config/keymaps.lua
+++ .config/LazyVim/lua/config/keymaps.lua
@@ -7,3 +7,9 @@ local map = vim.keymap.set
 -- ToggleTerm
 map("n", "<leader>bT", ":ToggleTerm direction=float <CR>", { desc = "Open terminal" })
 map("n", "<leader>bt", ":ToggleTerm direction=horizontal <CR>", { desc = "Open horizontal terminal split" })
+map(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume Telescope" }
+)
